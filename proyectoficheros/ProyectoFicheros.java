b7a0388e6e8b35f9d73f69734b401d427e94e258
adri4silva <adri4silva@gmail.com>
2017-03-29 09:14:10 +0200

    Menu para el programa

--- proyectoficheros/ProyectoFicheros.java
+++ proyectoficheros/ProyectoFicheros.java
@@ -5,6 +5,9 @@
  */
 package proyectoficheros;
 
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author asilvafernandez
@@ -14,8 +17,27 @@ public class ProyectoFicheros {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        
+        
+        int opt = Integer.parseInt(JOptionPane.showInputDialog("MENU:\n1. Crear alumnos\n2. Ordenar por aprobado\n3. Visualizar ArrayList\n4. Salir"));
+        while (opt>0 && opt<4){
+            switch (opt){
+                case 1: // Create the alumnos.dat file
+                    int nAlumnos = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de alumnos que desesas insertar"));
+                    Fichero.addAlumnoFichero(nAlumnos);
+                    break;
+                case 2: // Splits the alumnos.dat file in two different files aprobados.dat/suspensos.dat
+                    Fichero.addAoSFichero();
+                    break;
+                case 3: // Show the aprobados ArrayList
+                    Fichero.mostrarArrayList();
+                    break;
+                    
+            }
+            opt = Integer.parseInt(JOptionPane.showInputDialog("MENU:\n1. Crear alumnos\n2. Ordenar por aprobado\n3. Visualizar ArrayList\n4. Salir"));
+        }
+        
     }
     
 }
